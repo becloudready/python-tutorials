be3975b36923b189e70872e8c555bb6039504e05
Chandan Kumar <chandank@becloudready.com>
2020-02-14 21:26:21 -0500

    added bootcamp code

--- lab-03/pandas-np.py
+++ lab-03/pandas-np.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 20:37:49 2020
+Copyright: beCloudReady
+
+@author: Chandan Kumar
+"""
+
+
 import numpy as np
 import pandas as pd
 import csv
 import os
+
+print ("Hello World!")
+
 
 os.getcwd()
 
@@ -9,12 +22,9 @@ os.chdir('.')
 
 os.getcwd()
 
-
-
-
 #importing data from computer and other locations
 
-newdata = pd.read_csv('sample.csv')
+newdata = pd.read_csv('../dataset/sample.csv')
 
 #newdata = pd.read_csv('E:\\Shailendra\\Personal\\TR\\sample_data\\Sample_csv_File_V1.csv')
 
@@ -30,22 +40,11 @@ newdata.head()
 #getting frequency table of a single variable
 newdata['age'].value_counts()
 
-
-
 print(newdata.columns) # prints the column names
 
 newdata1=newdata
 # reading a csv file with options
 
-
-
-
-
-
-#Group by 
-
-
-newdata1.agg({'age':'mean'})
 
 
 print(newdata1['age'].mean())
@@ -54,10 +53,6 @@ print(newdata1['age'].median())
 
 print(newdata1['age'].std())
 
-
-#Defining a function
-
-#Example 1
 
 def greetings(name):
 	"""This function greets to the person passed in as	parameter"""
@@ -81,11 +76,10 @@ def cube(num):
 cube(9)
 cube(-9)
 
-#Parameters = Information can be passed to functions as parameter.
 
 def my_function(fname):
   print(fname + " is a Data Scientist")
 
-my_function("Bo")
+my_function("beCloudReady")
 my_function("Chandan")
 my_function("Shailendra")
