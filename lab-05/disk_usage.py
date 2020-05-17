ca3f007440fe345345d458c3d51fdb4fed5ce108
Chandan Kumar <chandank@becloudready.com>
2020-05-17 00:16:04 -0400

    fixed paramiko disk check and added subprocess

--- lab-05/disk_usage.py
+++ lab-05/disk_usage.py
@@ -1,30 +1,34 @@
 import paramiko
+import os
+from os.path import expanduser
 
-wrong change
 
-server_list=['localhost']
-mount_point = '/dev/mapper/fedora-root'
-threashold = 90
+
+server_list=['ec2-15-223-48-154.ca-central-1.compute.amazonaws.com']
+mount_point = '/dev/xvda1'
+threashold = 5
 
 def check_disk(server,mount_point):
+	k = paramiko.RSAKey.from_private_key_file(os.path.join(expanduser("~"),"ansible.pem"))
+	c = paramiko.SSHClient()
+	c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	s = paramiko.SSHClient()
 	s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	s.connect('localhost', username='chandank', password='chandan',
-		allow_agent=False,look_for_keys=False)
+	s.connect(server, username='ec2-user', pkey=k)
 	err,out,ins = s.exec_command("df -h")
 	ss=out.read()
-	for line in ss.split('\n'):
-		#print(line.split()[0])
+	for line in ss.decode().split('\n'):
+    		#print(line.split()[0])
 		if len(line.split()) > 0:
 			if line.split()[0] == mount_point:
 			#print(line.split()[0])
 				return int(line.split()[4].rstrip('%'))
 
 for server in server_list:
-	print(server)
+	print("Connecting to Server : {}".format(server))
 	disk_usage=check_disk(server,mount_point)
 	if disk_usage > threashold:
-		print('Sending email')
+		print('Disk {} is Full'.format(mount_point))
 	else:
 		print('All is well')
 
